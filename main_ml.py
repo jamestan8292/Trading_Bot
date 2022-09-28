2112986399fa6bf01c9880770b189b2b8b6d0437
James Tan <jamestan@Jamess-Mac-Studio.local>
2022-09-27 19:52:16 -0700

    Initial Commit

---
+++ main_ml.py
@@ -0,0 +1,7 @@
+# MAIN APPLICATION
+
+def main():
+    # do something
+
+if __name__ == "__main__":
+    main()
---
+++ technical_indicators_model.py
@@ -0,0 +1,9 @@
+# GENERATE TRADING SIGNALS USING TECHNICAL INDICATORS
+
+# Import the modules
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import confusion_matrix
+from imblearn.metrics import classification_report_imbalanced
