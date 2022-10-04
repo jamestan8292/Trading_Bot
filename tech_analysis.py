aca3dacdc0a207c840599940d40a7a31593aab31
Tash <taofik.sulaiman2@gmail.com>
2022-10-04 04:12:45 -0500

    Adding my finished Bolliner Band trading strategy -Taofik

---
+++ tech_analysis.py
@@ -0,0 +1,10 @@
+# Imports
+import pandas as pd
+import numpy as np
+from pathlib import Path
+import hvplot.pandas
+import matplotlib.pyplot as plt
+from sklearn import svm
+from sklearn.preprocessing import StandardScaler
+from pandas.tseries.offsets import DateOffset
+from sklearn.metrics import classification_report
